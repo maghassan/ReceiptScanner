139e0e65b4c851e02bdfea200d9cf6822e244b49
maghassan <elminu80@gmail.com>
2019-01-21 04:05:28 +0100

    Done with VisionImage class

--- app/src/main/java/com/bellokano/receiptscanner/VisionImage.java
+++ app/src/main/java/com/bellokano/receiptscanner/VisionImage.java
@@ -1,9 +1,15 @@
 package com.bellokano.receiptscanner;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
 import android.media.Image;
 import android.net.Uri;
+import android.os.Build;
+import android.util.Log;
 import android.util.SparseIntArray;
 import android.view.Surface;
 
@@ -12,6 +18,10 @@ import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+
+import androidx.annotation.RequiresApi;
+
+import static android.content.Context.CAMERA_SERVICE;
 
 public class VisionImage {
 
@@ -61,5 +71,44 @@ public class VisionImage {
         ORIENTATION.append(Surface.ROTATION_90, 0);
         ORIENTATION.append(Surface.ROTATION_180,    270);
         ORIENTATION.append(Surface.ROTATION_270,    180);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private int getRotationCompensation(String   cameraId, Activity  activity, Context context)
+        throws CameraAccessException    {
+        int deviceRotation  =   activity.getWindowManager().getDefaultDisplay().getRotation();
+        int rotationCompensation    =   ORIENTATION.get(deviceRotation);
+
+        CameraManager   cameraManager   =   (CameraManager)   context.getSystemService(CAMERA_SERVICE);
+        int sensorOrientation   =   cameraManager
+                .getCameraCharacteristics(cameraId)
+                .get(CameraCharacteristics.SENSOR_ORIENTATION);
+        rotationCompensation    =   (rotationCompensation   +   sensorOrientation   +   270)    %   360;
+
+        int result;
+        switch (rotationCompensation)   {
+            case    0:
+                result  =   FirebaseVisionImageMetadata.ROTATION_0;
+                break;
+            case    90:
+                result  =   FirebaseVisionImageMetadata.ROTATION_90;
+                break;
+            case    180:
+                result  =   FirebaseVisionImageMetadata.ROTATION_180;
+                break;
+            case    270:
+                result  =   FirebaseVisionImageMetadata.ROTATION_270;
+                break;
+            default:
+                result  =   FirebaseVisionImageMetadata.ROTATION_0;
+                Log.e(TAG,  "Bad rotation value: "  +   rotationCompensation);
+        }
+        return  result;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void getCompensation(Activity   activity, Context context)
+        throws CameraAccessException    {
+        int rotation    =   getRotationCompensation(MY_CAMERA_ID,   activity,   context);
     }
 }
